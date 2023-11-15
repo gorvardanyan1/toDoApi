7c9b33a49867b971732607d55773e8c0e88ca3f3
gorvardanyan1 <gorv887@gmail.com>
2023-11-15 19:56:40 +0400

    add corst 8

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: '*', credentials: true }));
 app.get("/", async (req, res) => {
     const cookie = req.cookies['jwt']
     if (cookie == undefined) {
