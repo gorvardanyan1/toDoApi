c46819c0dc44f18815a923bd26ffab9f51069416
gorvardanyan1 <gorv887@gmail.com>
2023-11-15 16:46:52 +0400

    add cors

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(cors({
     credentials: true,
-    origin: ["*"]
+    origin: ["https://to-do-next-iota.vercel.app"]
 }))
 app.get("/", async (req, res) => {
     const cookie = req.cookies['jwt']
